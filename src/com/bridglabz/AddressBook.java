2d255f5c6a23270a71eb84cb60eae40a82fe0197
shubhamSS9 <shubhamshinde0999@gmail.com>
2023-03-10 21:46:10 +0530

    added UC3_EditingExistingContactPersonByTheirName

--- src/com/bridglabz/AddressBook.java
+++ src/com/bridglabz/AddressBook.java
@@ -1,11 +1,14 @@
 package com.bridglabz;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    Scanner sc = new Scanner(System.in);
-
     public ContactPerson createContact() {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ContactPerson> addressBook1 = new ArrayList<>();
+
         ContactPerson person = new ContactPerson();
         System.out.print("Enter First Name: ");
         person.setFirstName(sc.next());
@@ -28,8 +31,59 @@ public class AddressBook {
     }
 
     public void addContact() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ContactPerson> addressBook1 = new ArrayList<>();
+
         ContactPerson contactPerson = createContact();
+        addressBook1.add(contactPerson);
         System.out.println(contactPerson);
         System.out.println("Contact added successfully");
+    }
+
+
+
+    public void editContact() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ContactPerson> addressBook1 = new ArrayList<>();
+
+        boolean isContactFound = false;
+        System.out.println("Enter Name to edit Contact");
+        String name = sc.next();
+        for (ContactPerson contactPerson : addressBook1) {
+            if (name.equalsIgnoreCase(contactPerson.getFirstName())) {
+                isContactFound = true;
+                System.out.print("Enter First Name :");
+                contactPerson.setFirstName(sc.next());
+                System.out.print("Enter Last Name :");
+                contactPerson.setLastName(sc.next());
+                System.out.print("Enter Address :");
+                contactPerson.setAddress(sc.next());
+                System.out.print("Enter City :");
+                contactPerson.setCity(sc.next());
+                System.out.print("Enter State :");
+                contactPerson.setState(sc.next());
+                System.out.print("Enter email :");
+                contactPerson.setEmail(sc.next());
+                System.out.print("Enter ZipCode :");
+                contactPerson.setZipCode(sc.nextInt());
+                System.out.print("Enter Phone Number :");
+                contactPerson.setPhoneNumber(sc.nextLong());
+                System.out.println(contactPerson);
+                break;
+            }
+        }
+        if (isContactFound) {
+            System.out.println("Contact Updated Successfully..");
+        } else {
+            System.out.println("Oops...Contact not found");
+        }
+    }
+
+    void display() {
+        ArrayList<ContactPerson> addressBook1 = new ArrayList<>();
+
+        for (ContactPerson person : addressBook1) {
+            System.out.println(person);
+        }
     }
 }
