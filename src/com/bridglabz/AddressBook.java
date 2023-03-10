d3b15fec28fae0bf21a13964d2e62bf02995a20a
shubhamSS9 <shubhamshinde0999@gmail.com>
2023-03-10 21:37:31 +0530

    added UC2_NewContactsAdded

--- src/com/bridglabz/AddressBook.java
+++ src/com/bridglabz/AddressBook.java
@@ -1,18 +1,35 @@
 package com.bridglabz;
 
-public class AddressBook {
-    public void createContact() {
+import java.util.Scanner;
 
-        ContactPerson person = new ContactPerson("shubham", "shinde", "LaxmiRoad", "Pune", "Maharashtra",
-                "dinesh123@gmail.com", 411025, "9404291279");
-        System.out.println("First Name: " + person.getFirstName());
-        System.out.println("Last Name: " + person.getLastName());
-        System.out.println("Address: " + person.getAddress());
-        System.out.println("City: " + person.getCity());
-        System.out.println("State: " + person.getState());
-        System.out.println("Email: " + person.getEmail());
-        System.out.println("Zip Code: " + person.getZipCode());
-        System.out.println("Phone Number: " + person.getPhoneNumber());
-        System.out.println("Contact created successfully.....");
+public class AddressBook {
+    Scanner sc = new Scanner(System.in);
+
+    public ContactPerson createContact() {
+        ContactPerson person = new ContactPerson();
+        System.out.print("Enter First Name: ");
+        person.setFirstName(sc.next());
+        System.out.print("Enter Last Name: ");
+        person.setLastName(sc.next());
+        System.out.print("Enter Address: ");
+        person.setAddress(sc.next());
+        System.out.print("Enter City: ");
+        person.setCity(sc.next());
+        System.out.print("Enter State: ");
+        person.setState(sc.next());
+        System.out.print("Enter email: ");
+        person.setEmail(sc.next());
+        System.out.print("Enter ZipCode: ");
+        person.setZipCode(sc.nextInt());
+        System.out.print("Enter Phone Number: ");
+        person.setPhoneNumber(sc.nextLong());
+        System.out.println("created new contact");
+        return person;
+    }
+
+    public void addContact() {
+        ContactPerson contactPerson = createContact();
+        System.out.println(contactPerson);
+        System.out.println("Contact added successfully");
     }
 }
--- src/com/bridglabz/AddressBookMain.java
+++ src/com/bridglabz/AddressBookMain.java
@@ -3,9 +3,7 @@ package com.bridglabz;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book program");
-        System.out.println("--------------------------");
-        System.out.println("Contact created in address book is: ");
         AddressBook addressBook = new AddressBook();
-        addressBook.createContact();
+        addressBook.addContact();
     }
 }
